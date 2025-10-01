05c8ece7f8f8976198d72af70ba8fe7c72607500
kaidranzerr <ashstark68@gmail.com>
2025-10-02 02:23:22 +0530

    fast api theory!

---
+++ FAST_API/fast_api1.py
@@ -0,0 +1,14 @@
+# L - 1
+# api are mechanisms that enable 2 software components such as the frontend and backend of an application to communicate with each 
+# other using a designed set of rules , protocols and data formats
+# APIs to enable different software systems to communicate and share data, allowing developers to integrate existing services rather 
+# than building everything from scratch. APIs act as intermediaries, defining rules for how applications interact, which promotes 
+# automation, innovation, security, and cost efficiency by simplifying software development and enabling seamless connectivity between 
+# various platforms and devices
+
+# L - 2
+# Fast API is a modern , high performance web framework for building APIs with python
+# Starlette --> manages how your API recieves requests and sends back messages
+# Pydantic -->  checks if the data coming into your API is correct and in the right format
+
+# philosophy of FastAPI --> fast to run and fast to code || web server for tracking incoming HTTP request of the client
